6e2f0a2899c2a63cefa1f56a75c6048d5ecf635e
Gaetan Almela <gaetan.almela@gmail.com>
2024-03-22 16:47:42 -0400

    working build & api

--- examples/simple/main.rs
+++ examples/simple/main.rs
@@ -1,15 +1,33 @@
+use std::time::SystemTime;
+
+use image::io::Reader as ImageReader;
+use ndarray::Ix2;
+use wfc::{data::{Flips, Rotations}, impls::image::ImageParams};
+
+// TODO: reexport needed types from image crate?
+
 fn main() {
-    // let sample_path = PathBuf::from("/home/al/files/github/wfc/examples/simple/sample.png");
+    let image = ImageReader::open("/home/al/files/github/wfc/examples/simple/sample.png")
+        .expect("couldn't find image")
+        .decode()
+        .expect("failed to decode the image");
 
-    // let bitmap = wfc::from_image(&sample_path, 3, true, true).expect("failed to create bitmap");
-    // let tileset = bitmap.tile_set();
-    //
-    // let t0 = SystemTime::now();
-    // let mut wave = tileset.wave(Ix2(10, 10));
-    // let t1 = SystemTime::now();
-    // wave.collapse(None);
-    // let t2 = SystemTime::now();
+    let image = image.into_rgb8();
 
-    // println!("sampled in {:?}", t1.duration_since(t0).unwrap());
-    // println!("collapsed in {:?}", t2.duration_since(t1).unwrap());
+    let params = ImageParams {
+        image,
+        win_size: 3
+    };
+
+    let mut tile_set = params.tile_set();
+    let mut wave = tile_set
+        .with_rots()
+        .with_flips()
+        .wave(Ix2(10, 10));
+
+    let t0 = SystemTime::now();
+    wave.collapse(None);
+    let t1 = SystemTime::now();
+
+    println!("collapsed in {:?}", t1.duration_since(t0).unwrap());
 }
