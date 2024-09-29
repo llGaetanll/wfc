c8b565fd7abfe13da6667d8ec7ef8d05792a85c1
Gaetan Almela <gaetan.almela@gmail.com>
2024-09-29 15:47:02 -0400

    Fix segfault in wrapping

--- examples/wrapping/main.rs
+++ examples/wrapping/main.rs
@@ -2,12 +2,9 @@ use std::fs;
 use std::path::PathBuf;
 use std::time::SystemTime;
 
-use image::DynamicImage;
 use image::ImageBuffer;
 use image::Pixel;
-use image::Rgba;
 use wfc::prelude::*;
-use wfc::surface::Surface;
 use wfc::wave::Wave;
 
 type Img<P> = ImageBuffer<P, Vec<<P as Pixel>::Subpixel>>;
@@ -26,16 +23,6 @@ fn scale_image<P: Pixel>(image: Img<P>, scale: u32) -> Img<P> {
     res
 }
 
-fn build_wave<S: Surface<2>>(
-    images: Vec<DynamicImage>,
-    shape: (usize, usize),
-) -> ImageWave<Rgba<u8>, S> {
-    let mut tileset = TileSet::from_images(images);
-    tileset.with_rots().with_flips();
-
-    Wave::init(&mut tileset, shape)
-}
-
 fn main() {
     let tiles_path = PathBuf::from("examples/samples/tiles/");
     let images: Vec<_> = fs::read_dir(tiles_path)
@@ -49,25 +36,31 @@ fn main() {
     let shape = (3, 3);
     let mut rng = rand::thread_rng();
 
+    // {
+    //     let mut tileset = TileSet::from_images(images.clone());
+    //     tileset.with_rots().with_flips();
+    //
+    //     let mut wave: ImageWave<_, Flat> = Wave::init(&mut tileset, shape);
+    //
+    //     let t0 = SystemTime::now();
+    //     let image = wave.collapse(&mut rng);
+    //     let t1 = SystemTime::now();
+    //
+    //     println!(
+    //         "collapsed flat wave in {:?}",
+    //         t1.duration_since(t0).unwrap()
+    //     );
+    //
+    //     println!("scaling image");
+    //     let image = scale_image(image, 10); // resize the image
+    //     image.save("flat.png").expect("failed to save image");
+    // }
+
     {
-        let mut wave = build_wave::<Flat>(images.clone(), shape);
+        let mut tileset = TileSet::from_images(images.clone());
+        tileset.with_rots().with_flips();
 
-        let t0 = SystemTime::now();
-        let image = wave.collapse(&mut rng);
-        let t1 = SystemTime::now();
-
-        println!(
-            "collapsed flat wave in {:?}",
-            t1.duration_since(t0).unwrap()
-        );
-
-        println!("scaling image");
-        let image = scale_image(image, 10); // resize the image
-        image.save("flat.png").expect("failed to save image");
-    }
-
-    {
-        let mut wave = build_wave::<Torus>(images.clone(), shape);
+        let mut wave: ImageWave<_, Torus> = Wave::init(&mut tileset, shape);
 
         let t0 = SystemTime::now();
         let image = wave.collapse(&mut rng);
@@ -83,41 +76,47 @@ fn main() {
         image.save("torus.png").expect("failed to save image");
     }
 
-    {
-        let mut wave = build_wave::<ProjectivePlane>(images.clone(), shape);
-
-        let t0 = SystemTime::now();
-        let image = wave.collapse(&mut rng);
-        let t1 = SystemTime::now();
-
-        println!(
-            "collapsed projective plane wave in {:?}",
-            t1.duration_since(t0).unwrap()
-        );
-
-        println!("scaling image");
-        let image = scale_image(image, 10); // resize the image
-        image
-            .save("projective-plane.png")
-            .expect("failed to save image");
-    }
-
-    {
-        let mut wave = build_wave::<KleinBottle>(images.clone(), shape);
-
-        let t0 = SystemTime::now();
-        let image = wave.collapse(&mut rng);
-        let t1 = SystemTime::now();
-
-        println!(
-            "collapsed klein bottle wave in {:?}",
-            t1.duration_since(t0).unwrap()
-        );
-
-        println!("scaling image");
-        let image = scale_image(image, 10); // resize the image
-        image
-            .save("klein-bottle.png")
-            .expect("failed to save image");
-    }
+    // {
+    //     let mut tileset = TileSet::from_images(images.clone());
+    //     tileset.with_rots().with_flips();
+    //
+    //     let mut wave: ImageWave<_, ProjectivePlane> = Wave::init(&mut tileset, shape);
+    //
+    //     let t0 = SystemTime::now();
+    //     let image = wave.collapse(&mut rng);
+    //     let t1 = SystemTime::now();
+    //
+    //     println!(
+    //         "collapsed projective plane wave in {:?}",
+    //         t1.duration_since(t0).unwrap()
+    //     );
+    //
+    //     println!("scaling image");
+    //     let image = scale_image(image, 10); // resize the image
+    //     image
+    //         .save("projective-plane.png")
+    //         .expect("failed to save image");
+    // }
+    //
+    // {
+    //     let mut tileset = TileSet::from_images(images.clone());
+    //     tileset.with_rots().with_flips();
+    //
+    //     let mut wave: ImageWave<_, KleinBottle> = Wave::init(&mut tileset, shape);
+    //
+    //     let t0 = SystemTime::now();
+    //     let image = wave.collapse(&mut rng);
+    //     let t1 = SystemTime::now();
+    //
+    //     println!(
+    //         "collapsed klein bottle wave in {:?}",
+    //         t1.duration_since(t0).unwrap()
+    //     );
+    //
+    //     println!("scaling image");
+    //     let image = scale_image(image, 10); // resize the image
+    //     image
+    //         .save("klein-bottle.png")
+    //         .expect("failed to save image");
+    // }
 }
