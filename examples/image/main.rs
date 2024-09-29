d857efc105ce9463fe8f025fc6b649b579b0469c
Gaetan Almela <gaetan.almela@gmail.com>
2024-09-29 10:52:35 -0400

    `example/image` now scales output image

--- examples/image/main.rs
+++ examples/image/main.rs
@@ -2,8 +2,26 @@ use std::env;
 use std::path::PathBuf;
 use std::time::SystemTime;
 
+use image::ImageBuffer;
+use image::Pixel;
 use wfc::prelude::*;
 use wfc::wave::traits::ParWave;
+
+type Img<P> = ImageBuffer<P, Vec<<P as Pixel>::Subpixel>>;
+
+fn scale_image<P: Pixel>(image: Img<P>, scale: u32) -> Img<P> {
+    let (width, height) = image.dimensions();
+    let mut res: Img<P> = ImageBuffer::new(width * scale, height * scale);
+
+    for i in 0..(width * scale) {
+        for j in 0..(height * scale) {
+            let pixel = image.get_pixel(i / scale, j / scale).to_owned();
+            res.put_pixel(i, j, pixel);
+        }
+    }
+
+    res
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -23,7 +41,7 @@ fn main() {
     let mut tileset = params.tileset();
     tileset.with_rots().with_flips();
 
-    let mut wave = ImageWave::init(&mut tileset, (80, 80));
+    let mut wave = ImageWave::init(&mut tileset, (10, 10));
 
     let mut rng = rand::thread_rng();
 
@@ -31,6 +49,7 @@ fn main() {
     let image = wave.collapse_parallel(&mut rng);
     let t1 = SystemTime::now();
 
+    let image = scale_image(image, 10); // resize the image
     image.save("wave.png").expect("failed to save image");
 
     println!("collapsed in {:?}", t1.duration_since(t0).unwrap());
